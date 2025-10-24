3b1dea540509ce6568354b7a9a9798d37e976100
AlejandroJarque <jarquealex0302@gmail.com>
2025-10-24 10:56:18 +0200

    refactoring consultas 6,7

--- queries/queries.sql
+++ queries/queries.sql
@@ -22,11 +22,14 @@ SELECT
 FROM producto;
 
 -- 6. Llista els noms (nombre) i els preus de tots els productes de la taula producto, convertint els noms a majúscula.
-SELECT UPPER(nombre) AS nombre_mayuscula, precio
+SELECT 
+	UPPER(nombre),
+    precio
 FROM producto;
-
 -- 7. Llista els noms (nombre) i els preus (precio) dels productes de la taula producto, convertint els noms a minúscula.
-SELECT LOWER(nombre) AS nombre_minuscula, precio
+SELECT 
+	LOWER(nombre),
+    precio
 FROM producto;
 
 -- 8.Llista el nom de tots els fabricants en una columna, i en una altra columna obtingui en majúscules els dos primers caràcters del nom del fabricant (iniciales).
