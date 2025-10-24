79795bf1b7b8ab062c772139e671979e8a965227
AlejandroJarque <jarquealex0302@gmail.com>
2025-10-24 11:21:41 +0200

    refactorign 6

--- queries/queries.sql
+++ queries/queries.sql
@@ -33,7 +33,7 @@ FROM producto;
 
 -- 6. Llista els noms (nombre) i els preus de tots els productes de la taula producto, convertint els noms a majúscula.
 SELECT 
-	UPPER(nombre) AS NOMBRE,
+	UPPER(nombre) AS nombre,
     precio
 FROM producto;
 
