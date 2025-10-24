212af9f41232840d48205b91217eae862a3ce8c2
AlejandroJarque <jarquealex0302@gmail.com>
2025-10-24 11:27:37 +0200

    refactoring consultas 9 y 10

--- queries/queries.sql
+++ queries/queries.sql
@@ -52,13 +52,13 @@ FROM fabricante;
 -- 9. Llista els noms i els preus dels productes, arrodonint el valor del preu (precio)
 SELECT 
 	nombre, 
-	ROUND(precio)
+	ROUND(precio) AS precio
 FROM producto;
 
 -- 10. Llista els noms i els preus de tots els productes (precio truncado) de la taula producto, truncant el valor del preu per a mostrar-lo sense cap xifra decimal.
 SELECT 
 	nombre, 
-	TRUNCATE(precio, 0)
+	TRUNCATE(precio, 0) AS precio truncado
 FROM producto;
 
 -- 11. Mostra una llista amb els codis dels fabricants que apareixen a la taula producto, incloent possibles repeticions.
