e4b2e15e8aca485310f008202a560b6a316e638e
AlejandroJarque <jarquealex0302@gmail.com>
2025-10-26 18:18:22 +0100

    querys 18, 19, 20, 21, 22

--- queries/queries.sql
+++ queries/queries.sql
@@ -101,19 +101,45 @@ SELECT
 LIMIT 2 OFFSET 3;
 
 -- 18. Llista el nom i el preu del producte més barat. (Utilitza solament les clàusules ORDER BY i LIMIT).
-
+SELECT 
+	producto.nombre,
+    producto.precio
+FROM producto
+ORDER BY precio ASC
+LIMIT 1
 
 -- 19. Llista el nom i el preu del producte més car. (Utilitza solament les clàusules ORDER BY i LIMIT).
-
+SELECT 
+	producto.nombre,
+    producto.precio
+FROM producto
+ORDER BY precio DESC
+LIMIT 1
 
 -- 20. Llista el nom de tots els productes del fabricant el codi de fabricant del qual és igual a 2.
-
+SELECT 
+	producto.nombre
+FROM producto
+WHERE codigo_fabricante = 2;
 
 -- 21. Retorna una llista amb el nom del producte, preu i nom de fabricant (nombre del fabricante) de tots els productes de la base de dades.
-
+SELECT 
+	producto.nombre,
+    producto.precio,
+    codigo_fabricante AS 'nombre del fabricante'
+FROM producto
+JOIN fabricante
+	ON producto.codigo_fabricante = fabricante.codigo;
 
 -- 22. Llista tots els productes amb nom, preu i nom del fabricant (nombre del fabricante) ordenats alfabèticament.
-
+SELECT 
+	producto.nombre,
+    producto.precio,
+    codigo_fabricante AS 'nombre del fabricante'
+FROM producto
+JOIN fabricante
+	ON producto.codigo_fabricante = fabricante.codigo
+ORDER BY producto.nombre ASC;
 
 -- 23. Retorna una llista amb el codi del producte, nom del producte, codi del fabricant (codigo fabricante) i nom del fabricant (nombre fabricante), de tots els productes de la base de dades.
 
